3ddd1dc16026ec74137e2942242ca118f8d7c962
hoangvietnguyen99 <hoangvietnguyen99@gmail.com>
2022-04-23 22:24:00 +0700

    Initial commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../Task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl: string = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+}
